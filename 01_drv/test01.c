9eb31df1bbec00cc7d4c9a789ba618bedf208712
mallius <mallius@qq.com>
2014-01-22 21:57:03 +0800

    01 driver

---
+++ 01_drv/test01.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+	int fd;
+	int buf;
+	fd = open("/dev/xxx", O_RDWR);
+	if(fd < 0)
+	{
+		printf("Can not open.\n");
+	}
+	read(fd, &buf, 1);
+
+
+
+	return 0;
+}
