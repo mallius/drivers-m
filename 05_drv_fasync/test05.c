c43ad627d48132f4363c9c5a87b1c4fccb25314c
mallius <mallius@qq.com>
2014-01-31 18:09:18 +0800

    05_drv_fasync（inter, waitqueue, 异步通知应用程序）

---
+++ 05_drv_fasync/test05.c
@@ -0,0 +1,47 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#include <poll.h>  	//poll
+#include <unistd.h>	//sleep()
+#include <signal.h>     //signal()
+#include <fcntl.h>	//fcntl()
+#include <sys/types.h>  //getpid()
+
+int fd;
+unsigned char key_val_buf;
+
+void my_signal_fun(int signum)
+{
+	read(fd, &key_val_buf, 1);
+	printf("key val:%d\n", key_val_buf);
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned char buf;
+	int ret;
+	int timeout = 2000;
+
+	fd = open("/dev/keys", O_RDWR);
+	if(fd < 0)
+	{
+		printf("Can not open.\n");
+	}
+
+	fcntl(fd, F_SETOWN, getpid());
+
+	int oflags;
+	oflags = fcntl(fd, F_GETFL);
+
+	fcntl(fd, F_SETFL, oflags|FASYNC);
+
+	signal(SIGIO, my_signal_fun);
+	while(1)
+	{
+		sleep(1000);
+	}
+
+	return 0;
+}
